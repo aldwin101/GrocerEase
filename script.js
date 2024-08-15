e9455fff804fe72cbc4340962fafebcf38a28224
aldwin <olano.aa@gmail.com>
2024-08-14 20:03:25 -0500

    Added alert message when input field is empty

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const itemInput = document.getElementById('listInput');
-const addItem = document.getElementById('addList');
+const addItemButton = document.getElementById('addList');
 const groceryList = document.getElementById('groceryList');
 
 // function for adding an item
 function addToList() {
     const item = itemInput.value.trim()
-
+    const message = document.getElementById('alertMessage')
     if (item) {
         createItem(item);
         itemInput.value = '';
+        message.textContent = ''
+    } else {
+        message.textContent = 'Please enter an item.'
     }
 }
 
-addItem.addEventListener('click', addToList)
+addItemButton.addEventListener('click', addToList)
 
 // function for creating an item 
 function createItem(item) {
@@ -23,3 +26,10 @@ function createItem(item) {
     groceryList.appendChild(newItem);
 }
 
+const groceryItems = () => {
+    
+    
+    JSON.stringify();
+
+} 
+localStorage.setItem('grocery_item', groceryItems);
